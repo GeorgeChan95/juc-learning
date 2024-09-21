479354d569431f39f7c4fa650a1971b6502f362c
George <george_95@126.com>
2024-09-21 08:44:57 +0800

    CompletableFuture 案例2

--- src/main/java/com/atguigu/juc/cf/CompletableFutureAPIDemo.java
+++ src/main/java/com/atguigu/juc/cf/CompletableFutureAPIDemo.java
@@ -6,10 +6,33 @@ import java.util.concurrent.*;
  * @auther zzyy
  * @create 2021-03-02 17:54
  */
-public class CompletableFutureAPIDemo
-{
-    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException
-    {
+public class CompletableFutureAPIDemo {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+        m5();
+    }
+
+    private static void m7() throws InterruptedException {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 20, 1L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(50), Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
+        CompletableFuture<Void> completableFuture = CompletableFuture.supplyAsync(() -> {
+//        try {TimeUnit.MILLISECONDS.sleep(20);} catch (InterruptedException e) {e.printStackTrace();}
+            System.out.println("1号任务" + "\t" + Thread.currentThread().getName());
+            return "abcd";
+        }, threadPoolExecutor).thenRunAsync(() -> {
+//        try {TimeUnit.MILLISECONDS.sleep(20);} catch (InterruptedException e) {e.printStackTrace();}
+            System.out.println("2号任务" + "\t" + Thread.currentThread().getName());
+        }).thenRun(() -> {
+//        try {TimeUnit.MILLISECONDS.sleep(20);} catch (InterruptedException e) {e.printStackTrace();}
+            System.out.println("3号任务" + "\t" + Thread.currentThread().getName());
+        }).thenRun(() -> {
+//        try {TimeUnit.MILLISECONDS.sleep(20);} catch (InterruptedException e) {e.printStackTrace();}
+            System.out.println("4号任务" + "\t" + Thread.currentThread().getName());
+        });
+
+        Thread.sleep(2000);
+        threadPoolExecutor.shutdown();
+    }
+
+    private static void m6() {
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 20, 1L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(50), Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
 
 
@@ -26,31 +49,46 @@ public class CompletableFutureAPIDemo
             return null;
         }).join());
 
-
-
-
         threadPoolExecutor.shutdown();
     }
 
     /**
      * thenCombine
      */
-    public static void m5()
-    {
-        System.out.println(CompletableFuture.supplyAsync(() -> {
+    public static void m5() throws ExecutionException, InterruptedException {
+        CompletableFuture<Integer> thenCombineResult = CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread().getName() + "\t" + "---come in 1");
             return 10;
         }).thenCombine(CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread().getName() + "\t" + "---come in 2");
             return 20;
-        }), (r1, r2) -> {
-            return r1 + r2;
-        }).join());
+        }), (x, y) -> {
+            System.out.println(Thread.currentThread().getName() + "\t" + "---come in 3");
+            return x + y;
+        }).thenCombine(CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread().getName() + "\t" + "---come in 4");
+            return 30;
+        }), (a, b) -> {
+            System.out.println(Thread.currentThread().getName() + "\t" + "---come in 5");
+            return a + b;
+        });
+        System.out.println("-----主线程结束，END");
+        System.out.println(thenCombineResult.get());
+
+
+        // 主线程不要立刻结束，否则CompletableFuture默认使用的线程池会立刻关闭:
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
     /**
      * 对计算速度进行选用
      */
-    public static void m4()
-    {
+    public static void m4() {
         System.out.println(CompletableFuture.supplyAsync(() -> {
             //暂停几秒钟线程
             try {
@@ -71,48 +109,51 @@ public class CompletableFutureAPIDemo
         }).join());
 
         //暂停几秒钟线程
-        try { TimeUnit.SECONDS.sleep(3); } catch (InterruptedException e) { e.printStackTrace(); }
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * 对计算结果进行消费
      */
-    public static void m3()
-    {
+    public static void m3() {
         CompletableFuture.supplyAsync(() -> {
             return 1;
         }).thenApply(f -> {
-            return f+2;
+            return f + 2;
         }).thenApply(f -> {
-            return f+3;
+            return f + 3;
         }).thenAccept(r -> System.out.println(r));
 
+        // 任务A执行完执行B，并且B不需要A的结果, 且B无返回值
+        System.out.println(CompletableFuture.supplyAsync(() -> "resultA").thenRun(() -> {
+        }).join());
 
-        System.out.println(CompletableFuture.supplyAsync(() -> "resultA").thenRun(() -> {}).join());
+        // 任务A执行完执行B，B需要A的结果，但是任务B无返回值
+        System.out.println(CompletableFuture.supplyAsync(() -> "resultA").thenAccept(r -> System.out.println(r)).join());
 
-
-        System.out.println(CompletableFuture.supplyAsync(() -> "resultA").thenAccept(resultA -> {}).join());
-
-
+        // 任务A执行完执行B，B需要A的结果，同时任务B有返回值
         System.out.println(CompletableFuture.supplyAsync(() -> "resultA").thenApply(resultA -> resultA + " resultB").join());
     }
 
     /**
      * 对计算结果进行处理
      */
-    public static void m2()
-    {
+    public static void m2() {
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 20, 1L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(50), Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
 
         System.out.println(CompletableFuture.supplyAsync(() -> {
             return 1;
-        }).handle((f,e) -> {
+        }).handle((f, e) -> {
             System.out.println("-----1");
             return f + 2;
-        }).handle((f,e) -> {
+        }).handle((f, e) -> {
             System.out.println("-----2");
             return f + 3;
-        }).handle((f,e) -> {
+        }).handle((f, e) -> {
             System.out.println("-----3");
             return f + 4;
         }).whenComplete((v, e) -> {
@@ -130,27 +171,36 @@ public class CompletableFutureAPIDemo
 
     /**
      * 获得结果和触发计算
+     *
      * @throws InterruptedException
      * @throws ExecutionException
      */
-    public static void m1() throws InterruptedException, ExecutionException
-    {
+    public static void m1() throws InterruptedException, ExecutionException {
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 20, 1L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(50), Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
 
         CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
             //暂停几秒钟线程
             //暂停几秒钟线程
-            try { TimeUnit.SECONDS.sleep(2); } catch (InterruptedException e) { e.printStackTrace(); }
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             return 1;
-        },threadPoolExecutor);
+        }, threadPoolExecutor);
 
         //System.out.println(future.get());
         //System.out.println(future.get(2L,TimeUnit.SECONDS));
         //暂停几秒钟线程
-        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         //System.out.println(future.getNow(9999));
 
-        System.out.println(future.complete(-44)+"\t"+future.get());
+        System.out.println(future.complete(-44) + "\t" + future.get());
 
 
         threadPoolExecutor.shutdown();
--- src/main/java/com/atguigu/juc/cf/CompletableFutureDemo.java
+++ src/main/java/com/atguigu/juc/cf/CompletableFutureDemo.java
@@ -7,14 +7,107 @@ import java.util.concurrent.*;
  * @auther zzyy
  * @create 2021-03-02 11:56
  */
-public class CompletableFutureDemo
-{
-    public static void main(String[] args)throws Exception
-    {
+public class CompletableFutureDemo {
+    public static void main(String[] args) throws Exception {
+//        thenApplyDemo();
+
+        handleDemo();
+    }
+
+private static void handleDemo() throws InterruptedException {
+    // 自定义线程池
+    ThreadPoolExecutor threadPool = new ThreadPoolExecutor(
+            4,
+            5,
+            50,
+            TimeUnit.SECONDS,
+            new LinkedBlockingQueue<>(10),
+            Executors.defaultThreadFactory(),
+            new ThreadPoolExecutor.AbortPolicy());
+
+    CompletableFuture.supplyAsync(() -> {
+        int result = ThreadLocalRandom.current().nextInt(10);
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (result > 1) {
+            int i = 10 / 0;
+        }
+        return 10;
+    }, threadPool).handle((v, e) -> {
+        if (e == null) {
+            System.out.println("handle1执行完成，结果：" + v);
+        } else {
+            System.out.println("handle1执行异常");
+        }
+        return v + 1;
+    }).handle((v, e) -> {
+        if (e == null) {
+            System.out.println("handle2执行完成，结果：" + v);
+        } else {
+            System.out.println("handle2执行异常");
+        }
+        return "abc";
+    }).handle((v, e) -> {
+        if (e == null) {
+            System.out.println("handle3执行完成，结果：" + v);
+        } else {
+            System.out.println("handle3执行异常");
+        }
+        return "xyz";
+    }).whenCompleteAsync((v, e) -> {
+        if (e == null) {
+            System.out.println(Thread.currentThread().getName() + " whenComplete执行完成，结果：" + v);
+        }
+    }, threadPool).exceptionally(e -> {
+        e.printStackTrace();
+        String msg = String.format("执行出现异常，原因：%s, 异常信息：%s", e.getCause(), e.getMessage());
+        System.out.println(msg);
+        return "error";
+    });
+
+    TimeUnit.SECONDS.sleep(5);
+
+    // 关闭线程池
+    threadPool.shutdown();
+
+}
+
+    private static void thenApplyDemo() throws InterruptedException {
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 20, 1L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(50), Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
 
+        CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(() -> {
+            int result = ThreadLocalRandom.current().nextInt(10);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
 
+            if (result > 5) {
+                int i = 10 / 0;
+            }
+            return 1;
+        }, threadPoolExecutor).thenApply(data -> data + 2).whenComplete((v, e) -> {
+            if (e == null) {
+                System.out.println("执行完成，结果：" + v);
+            }
+        }).exceptionally(e -> {
+            e.printStackTrace();
+            String msg = String.format("执行出现异常，原因：%s, 异常信息：%s", e.getCause(), e.getMessage());
+            System.out.println(msg);
+            return -1;
+        });
 
+        System.out.println("执行结束，结果：" + completableFuture.join());
+
+        // 关闭自定义线程池
         threadPoolExecutor.shutdown();
+
+        // 主线程停留3秒
+        TimeUnit.SECONDS.sleep(3);
     }
 }
